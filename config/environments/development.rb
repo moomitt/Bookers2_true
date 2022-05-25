8367c4ca28187f4d5d71ba8b3f6274715e4aca79
moomitt <100453545+moomitt@users.noreply.github.com>
2022-05-25 12:09:23 +0000

    [2] Gemの記載・インストール

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "ee81d80a429b412eaf11d2547a507929.vfs.cloud9.us-east-1.amazonaws.com"
 end
