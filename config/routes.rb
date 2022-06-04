4e7910a5249d7358dcddf85476f418ecf57960c3
moomitt <100453545+moomitt@users.noreply.github.com>
2022-06-04 14:02:52 +0000

    [30-31] Userのedit,update機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about', as: 'about'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   patch 'books/:id' => 'books#update', as: 'update_book'
+  patch 'users/:id' => 'users#update', as: 'update_user'
   resources :books, only: [:create, :index, :show, :edit, :destroy, :update]
   resources :users, only: [:index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
