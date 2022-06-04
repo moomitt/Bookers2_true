f5cd4b6cb7423b5e79c3ed2ec3f73c2d75e4d377
moomitt <100453545+moomitt@users.noreply.github.com>
2022-06-04 12:16:44 +0000

    [24-27] bookのshow, userのindex,showページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about', as: 'about'
-  resources :books, only: [:create, :index, :show, :destroy]
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  patch 'books/:id' => 'books#update', as: 'update_books'
+  resources :books, only: [:create, :index, :show, :edit, :destroy, :update]
   resources :users, only: [:index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
